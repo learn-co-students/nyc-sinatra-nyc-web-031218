05c4bf4f2e7727351051a5220c1baf25480a9812
Al <lin.alice72@gmail.com>
2018-04-05 16:46:09 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,8 +5,28 @@ class FiguresController < ApplicationController
     erb :"figures/index"
   end
 
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if params[:title]
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark]
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
+  end
+
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
+    @titles = @figure.titles
+    @landmarks = @figure.landmarks
     erb :"figures/show"
   end
 
@@ -18,13 +38,18 @@ class FiguresController < ApplicationController
   end
 
   patch '/figures/:id' do
-
     @figure = Figure.find(params[:id])
     if params[:figure][:title_ids]
       @figure.title_ids = params[:figure][:title_ids]
     end
       @figure.update(name: params[:figure][:name])
-      @figure.save
+    if params[:figure][:landmark_ids]
+      @figure.landmark_ids = params[:figure][:landmark_ids]
+    end
+    if params[:new_landmark]
+      @figure.landmark << Landmark.create(name: params[:new_landmark], figure_id: @figure.id)
+    end
+    @figure.save
     redirect "/figures/#{@figure.id}"
   end
 
