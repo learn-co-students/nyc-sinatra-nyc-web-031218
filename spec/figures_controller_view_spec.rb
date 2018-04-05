cc0fa107a06f48aeccc7a91bec1aeeba34800b77
JMoloughney <github email address>
2018-04-05 17:11:32 -0400

    Done.

--- spec/figures_controller_view_spec.rb
+++ spec/figures_controller_view_spec.rb
@@ -27,9 +27,11 @@ describe FiguresController do
   end
 
   it "allows you to create a new figure with a title" do
+
     visit '/figures/new'
     fill_in :figure_name, :with => "Doctor Who"
     check "title_#{Title.first.id}"
+
     click_button "Create New Figure"
     figure = Figure.last
     expect(Figure.all.count).to eq(3)
