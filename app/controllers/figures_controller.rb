6ebff08008c1c0a8c5df94d39f32d84ef0699dac
Helen Liutongco <liutongcoh@gmail.com>
2018-04-05 16:20:30 -0400

    Added files for Figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,63 @@
 class FiguresController < ApplicationController
 
-  
+  #read index
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  #get new figure form
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  #create new figure
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  #read individual figure
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  #read edit form
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    erb :"figures/edit"
+  end
+
+  #edit figure
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  #delete figure
+
+  delete '/figures/:id/delete' do
+    Figure.destroy(params[:id])
+    redirect to "/figures"
+  end
 
 end
